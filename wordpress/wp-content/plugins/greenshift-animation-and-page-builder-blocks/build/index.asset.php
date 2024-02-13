f4fb8602f9eca189492ba02e9e730b6f8fec277b
SnIvanova <1snizhanaivanova@gmail.com>
2024-02-13 15:19:49 +0100

    main config

---
+++ wordpress/wp-content/plugins/greenshift-animation-and-page-builder-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8ea6ecb51a160383ad68');
