7a1de492d939ee0631338e836b8d4d8a73b195f7
SnIvanova <1snizhanaivanova@gmail.com>
2024-02-13 16:29:16 +0100

    article page and links to all buttons

--- wordpress/wp-content/plugins/superb-blocks/blocks/ratings/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '1d3c6f23cc4b588c9a80');
